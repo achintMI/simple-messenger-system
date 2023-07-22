2880dc0a58ef0a766b99cf7ccdcc04065d82d0fb
achinta-mistry <97433901+achinta-mistry@users.noreply.github.com>
2023-07-23 03:39:26 +0530

    FEAT: login api added with jwt handling

--- src/models.py
+++ src/models.py
@@ -1,20 +1,22 @@
 import jwt
 from src.extensions import db
 from datetime import datetime, timedelta
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+
+TOKEN_SECRET_KEY = "token_secret_key"
 
 
 class Users(db.Model):
+
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     passcode = db.Column(db.String(128), nullable=False)
-    token = db.relationship('Tokens', backref='users', lazy=True)
 
     def generate_token(self):
         expiration = datetime.utcnow() + timedelta(hours=1)
         payload = {'user_id': self.id, 'exp': expiration}
-        token = jwt.encode(payload, 'some_secret_key', algorithm='HS256')
+        token = jwt.encode(payload, TOKEN_SECRET_KEY, algorithm='HS256')
         return token
 
     def __repr__(self):
@@ -22,15 +24,11 @@ class Users(db.Model):
 
     @property
     def password(self):
-        raise AttributeError('password is not a readable attribute')
+        return self.passcode
 
     @password.setter
     def password(self, passcode):
         self.passcode = generate_password_hash(passcode)
 
-
-class Tokens(db.Model):
-    __tablename__ = 'tokens'
-    id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String(200), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    def verify_password(self, passcode):
+        return check_password_hash(self.passcode, passcode)
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,7 @@
 import bcrypt
 from flask import Blueprint, request, jsonify
 from src.extensions import db
-from src.models import Users, Tokens
+from src.models import Users
 
 messaging_bp = Blueprint('messaging', __name__)
 
@@ -12,12 +12,31 @@ def create_user():
     username = request_data.get('username', None)
     passcode = request_data.get('passcode', None)
     if not username or not passcode:
-        return jsonify({'status': "failure", 'message': 'username or passcode is required'}), 400
+        return jsonify({'status': "failure", 'message': 'username and passcode are required'}), 400
 
     if Users.query.filter_by(username=username).first():
         return jsonify({'status': "failure", 'message': 'user already exists'}), 400
 
-    new_user = Users(username=username, passcode=passcode)
+    new_user = Users(username=username, password=passcode)
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'status': 'success'})
+
+
+@messaging_bp.route("/login", methods=["post"])
+def login():
+    request_data = request.get_json()
+    username = request_data.get('username', None)
+    passcode = request_data.get('passcode', None)
+
+    if not username or not passcode:
+        return jsonify({'status': 'failure', 'message': 'username and passcode are required'}), 400
+
+    user = Users.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({'status': 'failure', 'message': 'invalid credentials, user mot found'}), 401
+
+    if not user.verify_password(passcode):
+        return jsonify({'status': 'failure', 'message': 'invalid credentials, incorrect passcode'}), 401
+
+    return jsonify({'status': 'success', 'token': user.generate_token()}), 200
